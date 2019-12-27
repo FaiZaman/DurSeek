11c6571aafa65c971e484524fe7f7b730365af7a
faizzaman <faiz-uz.zaman@durham.ac.uk>
2019-12-27 17:46:20 +0500

    Added scrolling background and treasure

--- Game.py
+++ Game.py
@@ -4,12 +4,20 @@ from Player import Player
 
 pygame.init()
 
-# create game window, background, and clock
+# create game window and clock
 screen_width = 1024
 screen_height = 389
 window = Screen(screen_width, screen_height)
 window = window.create_screen()
+
+# load in background and character
 background = pygame.image.load("assets/background/cathedral.jpg")
+
+background_scroll_1 = 0
+background_scroll_2 = background.get_width()
+
+character = pygame.image.load("assets/character/standing/standing_1.png")
+treasure = pygame.image.load("assets/treasure.png")
 
 clock = pygame.time.Clock()
 
@@ -22,16 +30,33 @@ right_list = [pygame.image.load("assets/character/walk/R1.png"), pygame.image.lo
               pygame.image.load("assets/character/walk/R3.png"), pygame.image.load("assets/character/walk/R4.png"),\
               pygame.image.load("assets/character/walk/R5.png"), pygame.image.load("assets/character/walk/R6.png")]
 
-character = pygame.image.load("assets/character/standing/standing_1.png")
 
 # create player
 player = Player(100, 250, 64, 64)
+
+
+# draws all objects to window
+def draw_objects(window, player):
+
+    window.blit(background, (background_scroll_1, 0))
+    window.blit(background, (background_scroll_2, 0))
+    player.draw(window, left_list, right_list)
+    window.blit(treasure, (850, 300))
+    pygame.display.update()
+
 
 # main event loop
 running = True
 while running:
 
     clock.tick(18)
+
+    background_scroll_1 -= 5
+    background_scroll_2 -= 5
+    if background_scroll_1 < background.get_width() * -1:
+        background_scroll_1 = background.get_width()
+    if background_scroll_2 < background.get_width() * -1:
+        background_scroll_2 = background.get_width()
     
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -39,6 +64,7 @@ while running:
 
     keys = pygame.key.get_pressed()
         
+    # player movement
     if keys[pygame.K_LEFT] and player.x > player.speed:
         player.x -= player.speed
         player.walk_left = True
@@ -55,6 +81,7 @@ while running:
         player.standing = True
         player.steps = 0
 
+    # quadratic jumping functionality
     if not(player.is_jumping):
         if keys[pygame.K_SPACE]:
             player.is_jumping = True
@@ -72,9 +99,6 @@ while running:
             player.is_jumping = False
             player.jump_length = 8
 
-
-    window.blit(background, (0,0))
-    player.draw(window, left_list, right_list)
-    pygame.display.update()
+    draw_objects(window, player)
 
 pygame.quit()
