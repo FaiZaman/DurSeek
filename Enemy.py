a4a5fce13943025fd8a3e6aa6b3b8a779930b09c
Faiz <faiz-uz.zaman@dur.ac.uk>
2020-01-18 21:53:46 +0000

    Added black enemy and adjusted their speeds for variation

--- Enemy.py
+++ Enemy.py
@@ -1,23 +1,37 @@
 import pygame
+import random as rand
 from Entity import Entity
 
+black_list = []
 explosion_list = []
-for n in range(1, 91):
-    explosion_list.append(pygame.image.load("assets/explosions/E (" + str(n) + ").png"))
+
+for n in range(1, 44):
+    black_list.append(pygame.image.load("assets/black_enemy/L" + str(n) + ".png"))
+
+for m in range(1, 91):
+    explosion_list.append(pygame.image.load("assets/explosions/E (" + str(m) + ").png"))
 
 class Enemy(Entity):
 
-    left_list = [pygame.image.load("assets/enemy/L1.png"), pygame.image.load("assets/enemy/L2.png"),\
-                 pygame.image.load("assets/enemy/L3.png"), pygame.image.load("assets/enemy/L4.png"),\
-                 pygame.image.load("assets/enemy/L5.png"), pygame.image.load("assets/enemy/L6.png"),\
-                 pygame.image.load("assets/enemy/L7.png"), pygame.image.load("assets/enemy/L8.png"),\
-                 pygame.image.load("assets/enemy/L9.png"), pygame.image.load("assets/enemy/L10.png")]
+    red_list = [pygame.image.load("assets/red_enemy/L1.png"), pygame.image.load("assets/red_enemy/L2.png"),\
+                 pygame.image.load("assets/red_enemy/L3.png"), pygame.image.load("assets/red_enemy/L4.png"),\
+                 pygame.image.load("assets/red_enemy/L5.png"), pygame.image.load("assets/red_enemy/L6.png"),\
+                 pygame.image.load("assets/red_enemy/L7.png"), pygame.image.load("assets/red_enemy/L8.png"),\
+                 pygame.image.load("assets/red_enemy/L9.png"), pygame.image.load("assets/red_enemy/L10.png")]
+    
+    black_list = black_list
     explosion_list = explosion_list
 
     def __init__(self, x, y):
         
-        super().__init__(x, y, self.left_list[0])
-        self.speed = 3
+        self.is_red = True
+        if rand.random() >= 0.33:
+            super().__init__(x, y, self.red_list[0])
+            self.speed = 2
+        else:
+            super().__init__(x, y, self.black_list[0])
+            self.is_red = False
+            self.speed = 4
         self.steps = 0
         self.exploding = False
         self.explode_count = 0
@@ -26,10 +40,16 @@ class Enemy(Entity):
     def set_image(self):
         
         if not(self.exploding):
-            if self.steps + 1 > 30:
-                self.steps = 0
 
-            self.image = self.left_list[self.steps//3]
+            if self.is_red:
+                if self.steps + 1 > 30:
+                    self.steps = 0
+                self.image = self.red_list[self.steps//3]
+            else:
+                if self.steps + 1 > 129:
+                    self.steps = 0
+                self.image = self.black_list[self.steps//3]
+            
             self.steps += 1
             self.rect.x -= self.speed
         else:
